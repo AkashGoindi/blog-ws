daa07f8464ea1ab98060777fff9e5eb4732719e8
Akash Goindi <41852582+AkashGoindi@users.noreply.github.com>
2022-01-18 18:33:51 +0530

    Dynamic Port

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const schema = require('./schemas/index.js');
 const isAuth = require('./middleware/auth');
 
+const PORT = process.env.PORT || 40040;
+
 const app = express();
 // const root = { hello: () => 'Hello world!' };
 var root = {
@@ -33,7 +35,7 @@ console.log('process.env.MONGO_USER', process.env.MONGO_USER, process.env.MONGO_
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.8erqx.mongodb.net/${process.env.MONGO_NAME}?retryWrites=true&w=majority`
 ).then(
-  app.listen(80, () => console.log('Active on: localhost:80/graphql'))
+  app.listen(PORT, () => console.log(`Active on: localhost:${ PORT }/graphql`))
 ).catch(err => {
   console.log("Error", err)
 });
